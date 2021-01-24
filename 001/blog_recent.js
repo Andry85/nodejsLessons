a62016667219e493169058be38a34e6fcd6815cc
Andrii Shapoval <andrii.shapoval@smile-ukraine.com>
2021-01-24 19:17:11 +0200

    added telnet

--- 001/blog_recent.js
+++ 001/blog_recent.js
@@ -1,26 +1,39 @@
 const http = require('http');
 const fs = require('fs');
 
-http.createServer((req,res) => {
-    if (req.url == '/') {
-        fs.readFile('./title.json', (err, data) => {
-            if (err) {
-                console.error(err);
-                res.end('Server Errorr');
-            } else {
-                const titles = JSON.parse(data.toString());
-                fs.readFile('./template.html', (err, data) => {
-                    if (err) {
-                        console.error(err);
-                        res.end('Server Errorr');
-                    } else {
-                        const tmpl = data.toString();
-                        const html = tmpl.replace('%', titles.join('</li><li>'));
-                        res.writeHead(200, {'Content-Type' : 'text/html'});
-                        res.end(html);
-                    }
-                });
-            }
-        });
-    }
+http.createServer((req, res) => {
+    getTitles(res);
 }).listen(9000, '127.0.0.1');
+
+function getTitles(res) {
+    fs.readFile('./title.json', (err, data) => {
+        if (err) {
+            handError(err, res)
+        } else {
+            getTemplate(JSON.parse(data.toString()), res);
+        }
+    });
+}
+
+function getTemplate(titles, res) {
+    fs.readFile('./template.html', (err, data) => {
+        if (err) {
+            handError(err, res)
+        } else {
+            console.log(data);
+            console.log(data.toString());
+            formatHtml(titles, data.toString(), res);
+        }
+    });
+}
+
+function formatHtml(`titles`, tmpl, res) {
+    const html = tmpl.replace('%', titles.join('</li><li>'));
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+    res.end(html);
+}
+
+function handError(err, res) {
+    console.error(err);
+    res.end('Server Errorr');
+}
