6dd00c166b5673df0933567c0b7f34ed2b471414
Andrii Shapoval <andrii.shapoval@smile-ukraine.com>
2021-03-02 07:51:16 +0200

    added changes

--- modulesapp/app.js
+++ modulesapp/app.js
@@ -1,31 +1,10 @@
-// получаем модуль Express
-const express = require("express");
-// создаем приложение
-const app = express();
- 
-// устанавливаем обработчик для маршрута "/"
-app.get("/", function(request, response){
- 
-    response.end("Hello from Expre!");
-});
-// начинаем прослушивание подключений на 3000 порту
-app.listen(3000);
-
-function displaySync(callback){
-    callback();
-}
- 
-console.log("Начало работы программы");
- 
-setTimeout(function(){
-         
-        console.log("timeout 500");
-}, 500);
- 
-setTimeout(function(){
-         
-        console.log("timeout 100");
-}, 100);
- 
-displaySync(function(){console.log("without timeout")});
-console.log("Завершение работы программы");
+const fs = require("fs");
+const zlib = require("zlib");
+  
+let readableStream = fs.createReadStream("hello.txt", "utf8");
+  
+let writeableStream = fs.createWriteStream("hello.txt.gz");
+  
+let gzip = zlib.createGzip();
+  
+readableStream.pipe(gzip).pipe(writeableStream);
